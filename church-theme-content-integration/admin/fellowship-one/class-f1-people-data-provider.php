47cb7f92de1336a865851d29e5c6a4be590de050
Chris Burgess <chris.burgess@acuit.com.au>
2014-04-04 08:06:56 +1100

    Initial f1 people data provider

---
+++ church-theme-content-integration/admin/fellowship-one/class-f1-people-data-provider.php
@@ -0,0 +1,167 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Chris
+ * Date: 28/02/14
+ * Time: 11:59 AM
+ */
+
+require_once dirname( __FILE__ ) . '/../interface-people-data-provider.php';
+require_once dirname( __FILE__ ) . '/../class-people-group.php';
+require_once dirname( __FILE__ ) . '/../class-person.php';
+
+class CTCI_F1PeopleDataProvider implements CTCI_PeopleDataProviderInterface {
+
+	protected $settings;
+
+	protected $authClient;
+
+	protected $logger;
+
+	/*protected $syncGroups = true;
+	protected $peopleListsToSync = array();*/
+
+	public function __construct(
+		CTCI_F1OAuthClientInterface $authClient,
+		CTCI_F1PeopleSyncSettingsInterface $settings,
+		CTCI_LoggerInterface $logger
+	) {
+		$this->settings = $settings;
+		$this->authClient = $authClient;
+		$this->logger = $logger;
+		/*$this->syncGroups = $settings->f1SyncPeopleGroups();
+		$this->peopleListsToSync = $settings->getF1PeopleLists();*/
+	}
+
+	/**************************************
+	 *
+	 * People Sync Methods
+	 *
+	 *************************************/
+
+	/** @var CTCI_PersonInterface[]  */
+	protected $people = array();
+
+	/** @var CTCI_PeopleGroupInterface[]  */
+	protected $groups = array();
+
+	public function getProviderPersonTag() {
+		return 'f1';
+	}
+
+	public function syncGroups() {
+		return $this->settings->f1SyncPeopleGroups();
+	}
+
+	public function setupForPeopleSync() {
+		$this->people = array();
+		$this->groups = array();
+		$this->retrievePeopleData();
+	}
+
+	protected function retrievePeopleData() {
+		// TODO: look at handling provider errors more closely
+		$peopleListsToSync = $this->settings->getF1PeopleLists();
+		// maintain an internal list of groups here, indexed by name for easy list searching
+		//$groupList = array();
+		$this->authClient->json();
+		$peopleLists = json_decode( $this->authClient->getPeopleLists() );
+		if ( null === $peopleLists ) {
+			throw new CTCI_JSONDecodeException;
+		}
+		foreach ( $peopleLists->peopleLists->peopleList as $peopleList ) {
+			if ( in_array( $peopleList->name, $peopleListsToSync ) ) {
+				$this->logger->info( 'Processing List: ' . $peopleList->name );
+
+				// we have a people list to sync
+				$currGroup = new CTCI_PeopleGroup(
+					$this->getProviderPersonTag(),
+					$peopleList->{'@id'},
+					$peopleList->name,
+					$peopleList->description
+				);
+				$this->groups[ $peopleList->{'@id'} ] = $currGroup;
+					// scan for members
+				$members = json_decode( $this->authClient->getPeopleListMembers( $peopleList->{'@id'} ) );
+				if ( null === $members ) {
+					throw new CTCI_JSONDecodeException;
+				}
+				foreach ( $members->members->member as $member ) {
+					// note, there is a member id, and a person id. We want the person id.
+					$personId = $member->person->{'@id'};
+					if ( isset( $this->people[ $personId ] ) ) {
+						// this person might have already been added if they are a member of another
+						// people list already processed.
+						$this->people[ $personId ]->addGroup( $currGroup );
+						continue;
+					}
+					$personData = $this->authClient->getPerson( $personId );
+					$personData = json_decode( $personData );
+					if ( null === $personData ) {
+						throw new CTCI_JSONDecodeException;
+					}
+					$personData = $personData->person;
+					$person = new CTCI_Person( $this->getProviderPersonTag(), $personData->{'@id'} );
+					$nameFormat = $this->settings->f1NameFormat();
+					if ( ! empty( $nameFormat ) ) {
+						$person->setNameFormat( $nameFormat );
+					}
+					if ( ! empty( $personData->title ) ) {
+						$person->setTitle( $personData->title );
+					}
+					if ( ! empty( $personData->prefix ) ) {
+						$person->setNamePrefix( $personData->prefix );
+					}
+					if ( ! empty( $personData->firstName ) ) {
+						$person->setFirstName( $personData->firstName );
+					}
+					if ( ! empty( $personData->goesByName ) ) {
+						$person->setGoesByName( $personData->goesByName );
+					}
+					if ( ! empty( $personData->middleName ) ) {
+						$person->setMiddleName( $personData->middleName );
+					}
+					if ( ! empty( $personData->lastName ) ) {
+						$person->setLastName( $personData->lastName );
+					}
+					if ( ! empty( $personData->suffix ) ) {
+						$person->setNameSuffix( $personData->suffix );
+					}
+					$person->addGroup( $currGroup );
+					$this->people[ $person->id() ] = $person;
+				}
+			}
+		}
+	}
+
+	/**
+	 * @return CTCI_PersonInterface[]
+	 */
+	public function getPeople() {
+		return $this->people;
+	}
+
+	/**
+	 * Return the groups defined in the data provider in the form of an array, where each element is an
+	 * array containing 'id' and 'name'.
+	 * @return CTCI_PeopleGroupInterface[]
+	 */
+	public function getGroups() {
+		return $this->groups;
+	}
+
+	public function cleanUpAfterPeopleSync() {
+
+	}
+
+	public function deleteUnattachedGroups() {
+		return false;   // TODO: make option
+	}
+
+	public function deleteUnattachedPeople() {
+		return false;   // TODO: make option
+	}
+}
+
+class CTCI_JSONDecodeException extends Exception {}
--- tests/church-theme-content-integration/admin/fellowship-one/class-f1-people-data-provider-test.php
+++ tests/church-theme-content-integration/admin/fellowship-one/class-f1-people-data-provider-test.php
@@ -6,8 +6,324 @@
  * Time: 12:19 PM
  */
 
+require_once dirname( __FILE__ ) . '/../../../../church-theme-content-integration/admin/fellowship-one/class-f1-people-data-provider.php';
+require_once dirname( __FILE__ ) . '/../../../../church-theme-content-integration/admin/fellowship-one/fellowship-one.php';
+require_once dirname( __FILE__ ) . '/../../../../church-theme-content-integration/admin/fellowship-one/OAuth/class-f1-oauth-client.php';
+require_once dirname( __FILE__ ) . '/../../../../church-theme-content-integration/admin/class-logger.php';
 
 class CTCI_F1PeopleDataProviderTest extends \PHPUnit_Framework_TestCase {
 
+	/** @var PHPUnit_Framework_MockObject_MockObject */
+	protected $authClientMock;
+	/** @var PHPUnit_Framework_MockObject_MockObject */
+	protected $peopleSyncSettingsMock;
+	/** @var PHPUnit_Framework_MockObject_MockObject */
+	protected $loggerMock;
+
+	/**
+	 * @var CTCI_F1PeopleDataProvider
+	 */
+	protected $sutF1PeopleDataProvider;
+
+	public function setUp() {
+		$this->authClientMock = $this->getMockBuilder('CTCI_F1OAuthClient')
+			->disableOriginalConstructor()
+			->getMock();
+
+		$this->authClientMock
+			->expects($this->any())
+			->method('json')
+			->will($this->returnSelf());
+
+		$this->peopleSyncSettingsMock = $this->getMockBuilder('CTCI_Fellowship_One')
+			->disableOriginalConstructor()
+			->getMock();
+
+		$this->loggerMock = $this->getMockBuilder('CTCI_Logger')
+			->disableOriginalConstructor()
+			->getMock();
+
+		$this->sutF1PeopleDataProvider = new CTCI_F1PeopleDataProvider(
+			$this->authClientMock, $this->peopleSyncSettingsMock, $this->loggerMock
+		);
+	}
+
+	public function testSetupForPeopleSync_1() {
+		// todo: add communications
+		/*
+		 * Sync groups true
+		 * 1 people list to sync, called Staff
+		 */
+		$this->peopleSyncSettingsMock
+			->expects($this->any())
+			->method('f1SyncPeopleGroups')
+			->will($this->returnValue(true));
+		$this->peopleSyncSettingsMock
+			->expects($this->any())
+			->method('getF1PeopleLists')
+			->will($this->returnValue(array('Staff')));
+		$groupsJSON = $this->buildPeopleListsJSON( array(
+			array(
+				'id' => '1234',
+				'name' => 'Group Leaders',
+			),
+			array(
+				'id' => '1235',
+				'name' => 'Staff',
+				'description' => 'All full-time staff'
+			)
+		));
+		// check that the test json data is well formed
+		$this->assertNotNull( json_decode( $groupsJSON ) );
+		$this->authClientMock
+			->expects($this->any())
+			->method('getPeopleLists')
+			->will($this->returnValue( $groupsJSON ));
+		$memberListJSON = $this->buildMemberListJSON( array(
+			'id' => '1235', 'name' => 'Staff'
+		), array(
+			array(
+				'id' => '12345',
+				'person-id' => '123456',
+				'name' => 'Steve Jobs'
+			), array(
+				'id' => '12346',
+				'person-id' => '123457',
+				'name' => 'Max Once'
+			), array(
+				'id' => '12347',
+				'person-id' => '123458',
+				'name' => 'John Doe'
+			)
+		));
+		$this->assertNotNull( json_decode( $memberListJSON ) );
+		$this->authClientMock
+			->expects($this->any())
+			->method('getPeopleListMembers')
+			->will($this->returnValue($memberListJSON));
+		$person1JSON = $this->buildPersonJSON( array(
+			'id' => '123456',
+			'title' => 'Dr',
+			'firstName' => 'Steve',
+			'lastName' => 'Jobs'
+		));
+		$this->assertNotNull( json_decode( $person1JSON ) );
+		$person2JSON = $this->buildPersonJSON( array(
+			'id' => '123457',
+			'firstName' => 'Max',
+			'lastName' => 'Once'
+		));
+		$this->assertNotNull( json_decode( $person2JSON ) );
+		$person3JSON = $this->buildPersonJSON( array(
+			'id' => '123458',
+			'title' => 'Mr',
+			'firstName' => 'Johnathan',
+			'lastName' => 'Doe',
+			'middleName' => 'Edward',
+			'goesByName' => 'John',
+			'suffix' => 'II'
+		));
+		$this->assertNotNull( json_decode( $person3JSON ) );
+		$this->authClientMock
+			->expects($this->any())
+			->method('getPerson')
+			->will($this->returnValueMap(array(
+				array( 123456, $person1JSON ), array( 123457, $person2JSON ), array( 123458, $person3JSON ),
+				array( '123456', $person1JSON ), array( '123457', $person2JSON ), array( '123458', $person3JSON )
+			)));
+
+		try {
+			$this->sutF1PeopleDataProvider->setupForPeopleSync();
+		} catch ( Exception $e ) {
+			/** @noinspection PhpUnusedLocalVariableInspection */
+			$x = 'stop';
+			throw $e;
+		}
+
+		$groups = $this->sutF1PeopleDataProvider->getGroups();
+		$people = $this->sutF1PeopleDataProvider->getPeople();
+
+		$this->assertInternalType( 'array', $groups );
+		$this->assertCount( 1, $groups );
+		$this->assertArrayHasKey( 1235, $groups );
+		$group = $groups[ 1235 ];
+		$this->assertInstanceOf( 'CTCI_PeopleGroupInterface', $group );
+		$this->assertEquals( 1235, $group->id() );
+		$this->assertEquals( 'Staff', $group->getName() );
+		$this->assertEquals( 'All full-time staff', $group->getDescription() );
+
+		$this->assertInternalType( 'array', $people );
+		$this->assertCount( 3, $people );
+		$this->assertArrayHasKey( 123456, $people );
+		$this->assertArrayHasKey( 123457, $people );
+		$this->assertArrayHasKey( 123458, $people );
+		$person1 = $people[123456];
+		$this->assertInstanceOf( 'CTCI_PersonInterface', $person1 );
+		$this->assertEquals( 123456, $person1->id() );
+		$this->assertEquals( 'Dr Steve Jobs', $person1->getName( 'T F L' ) );
+		$person1Groups = $person1->getGroups();
+		$this->assertCount( 1, $person1Groups );
+		$firstGroup = reset( $person1Groups );
+		$this->assertEquals( 1235, $firstGroup->id() );
+
+		$person2 = $people[123457];
+		$this->assertInstanceOf( 'CTCI_PersonInterface', $person2 );
+		$this->assertEquals( 123457, $person2->id() );
+		$this->assertEquals( 'Max Once', $person2->getName( 'F L' ) );
+		$person2Groups = $person2->getGroups();
+		$this->assertCount( 1, $person2Groups );
+		$firstGroup = reset( $person2Groups );
+		$this->assertEquals( 1235, $firstGroup->id() );
+
+		$person3 = $people[123458];
+		$this->assertInstanceOf( 'CTCI_PersonInterface', $person3 );
+		$this->assertEquals( 123458, $person3->id() );
+		$this->assertEquals( 'Mr Johnathan "John" Edward Doe II', $person3->getName( 'T FQ M L S' ) );
+		$person3Groups = $person3->getGroups();
+		$this->assertCount( 1, $person3Groups );
+		$firstGroup = reset( $person3Groups );
+		$this->assertEquals( 1235, $firstGroup->id() );
+
+	}
+
+	protected function buildPeopleListsJSON( $lists ) {
+		$json = '{"peopleLists":{"peopleList":[';
+		foreach ( $lists as $list ) {
+			$json .= sprintf('{"@id":"%1$s","@uri":"https://demo.staging.fellowshiponeapi.com/v1/people/lists/%1$s",', $list['id'] );
+			$json .= sprintf('"name":"%s",', $list['name']);
+			if ( isset( $list['description'] ) ) {
+				$description = sprintf( '"%s"', $list['description'] );
+			} else {
+				$description = 'null';
+			}
+			$json .= sprintf('"description":%s,', $description);
+			$json .= '"startDate":"2008-01-16T00:12:43",
+				"isOpen":"true",
+				"createdDate":"2005-04-15T10:47:56",
+				"createdByPerson":{
+				"@id":"5807",
+					"@uri":"https://demo.staging.fellowshiponeapi.com/v1/people/5807"
+				},
+				"lastUpdatedDate":null,
+				"lastUpdatedByPerson":{
+				"@id":"",
+				"@uri":""
+			}';
+			$json .= '},';
+		}
+		// remove last comma
+		$json = substr( $json, 0, strlen( $json ) - 1 );
+		$json .= ']}}';
+		return $json;
+	}
+
+	protected function buildMemberListJSON( $list, $members ) {
+		$json = '{"members":{"member":[';
+		foreach ( $members as $member ) {
+			$json .= sprintf('{"@id":"%2$s","@uri":"https://demo.staging.fellowshiponeapi.com/v1/people/lists/%1$s/members/%2$s",', $list['id'], $member['id'] );
+			$json .= sprintf(
+				'"peopleList":{
+					"@id":"%1$s",
+					"@uri":"https://demo.staging.fellowshiponeapi.com/v1/people/lists/%1$s",
+					"name":"%2$s"
+				},', $list['id'], $list['name'] );
+			$json .= sprintf(
+				'"person":{
+					"@id":"%1$s",
+					"@uri":"https://demo.staging.fellowshiponeapi.com/v1/people/%1$s",
+					"name":"%2$s"
+				},', $member['person-id'], $member['name'] );
+			$json .= '"createdDate":"2012-06-27T11:49:55",
+				"createdByPerson":{
+					"@id":"1649898",
+					"@uri":"https://demo.staging.fellowshiponeapi.com/v1/people/1649898"
+				},
+				"lastUpdatedDate":"2013-10-22T18:53:01",
+				"lastUpdatedByPerson":{
+					"@id":"",
+					"@uri":""
+				}';
+			$json .= '},';
+		}
+		// remove last comma
+		$json = substr( $json, 0, strlen( $json ) - 1 );
+		$json .= ']}}';
+		return $json;
+	}
+
+	protected function buildPersonJSON( $person ) {
+		$json = '{"person":{';
+		$json .= sprintf('"@id":"%s",', $person['id']);
+		$json .= sprintf('"@uri":"https://demo.fellowshiponeapi.com/v1/People/%s",', $person['id']);
+		$json .= '"@imageURI": "",
+        "@oldID": "",
+        "@iCode": "rD94LTAKe4ROXa50SLH7oQ==",
+        "@householdID": "14338522",
+        "@oldHouseholdID": "",';
+		$json .= sprintf('"title":%s,', isset( $person['title'] ) ? '"' . $person['title'] . '"' : 'null' );
+		$json .= sprintf('"salutation":%s,', isset( $person['salutation'] ) ? '"' . $person['salutation'] . '"' : 'null' );
+		$json .= sprintf('"prefix":%s,', isset( $person['prefix'] ) ? '"' . $person['prefix'] . '"' : 'null' );
+		$json .= sprintf('"firstName":%s,', isset( $person['firstName'] ) ? '"' . $person['firstName'] . '"' : 'null' );
+		$json .= sprintf('"lastName":%s,', isset( $person['lastName'] ) ? '"' . $person['lastName'] . '"' : 'null' );
+		$json .= sprintf('"suffix":%s,', isset( $person['suffix'] ) ? '"' . $person['suffix'] . '"' : 'null' );
+		$json .= sprintf('"middleName":%s,', isset( $person['middleName'] ) ? '"' . $person['middleName'] . '"' : 'null' );
+		$json .= sprintf('"goesByName":%s,', isset( $person['goesByName'] ) ? '"' . $person['goesByName'] . '"' : 'null' );
+		$json .= '"formerName": null,
+        "gender": "Male",
+        "dateOfBirth": "1976-08-13T00:00:00",
+        "maritalStatus": "Married",
+        "householdMemberType": {
+            "@id": "1",
+            "@uri": "https://demo.fellowshiponeapi.com/v1/People/HouseholdMemberTypes/1",
+            "name": "Head"
+        },
+        "isAuthorized": "true",
+        "status": {
+            "@id": "1",
+            "@uri": "https://demo.fellowshiponeapi.com/v1/People/Statuses/1",
+            "name": "Member",
+            "comment": null,
+            "date": null,
+            "subStatus": {
+                "@id": "",
+                "@uri": "",
+                "name": null
+            }
+        },
+        "occupation": {
+            "@id": "",
+            "@uri": "",
+            "name": null,
+            "description": null
+        },
+        "employer": "Fellowship Technologies",
+        "school": {
+            "@id": "",
+            "@uri": "",
+            "name": null
+        },
+        "denomination": {
+            "@id": "",
+            "@uri": "",
+            "name": null
+        },
+        "formerChurch": null,
+        "barCode": "dfhg",
+        "memberEnvelopeCode": "12341234",
+        "defaultTagComment": null,
+        "weblink": {
+            "userID": null,
+            "passwordHint": null,
+            "passwordAnswer": null
+        },
+        "solicit": null,
+        "thank": null,
+        "firstRecord": "2008-12-11T07:41:07",
+        "lastMatchDate": null,
+        "createdDate": "2008-12-11T07:41:08",
+        "lastUpdatedDate": "2009-04-30T15:12:49"';
+		$json .= '}}';
+		return $json;
+	}
 }
  
